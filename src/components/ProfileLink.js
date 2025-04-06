847c43154200ad91869717980bfa2f20801aaaf3
theo <thjmaurino@gmail.com>
2025-04-05 21:36:40 -0500

    mmmm

--- src/components/ProfileLink.js
+++ src/components/ProfileLink.js
@@ -1,11 +1,24 @@
 "use client"
 import { useAuth } from '@/context/authContext'
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
 
 const ProfileLink = () => {
     const {user} = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(!user) {
+            router.push('/');
+        }
+    }, [user])
+
+    function handleProfileClick() {
+        router.push('/profile');
+    }
+
   return (
-    <div className='profile-picture-and-link absolute top-3 right-3 w-[40px] h-[40px]'>
+    <div className='profile-picture-and-link absolute top-3 right-3 w-[40px] h-[40px]' onClick={handleProfileClick}>
         <img src={user.photoURL} className='profile-photo rounded-full' />
     </div>
   )
